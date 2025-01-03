8b91ad611e063216fe2f33474539d3abd36ae1f8
yahya-projet <yahyabenkhi8@gmail.com>
2025-01-03 23:53:57 +0100

    crer connect database

---
+++ server/database/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+export const conn = async() =>{
+    try {
+        await mongoose.connect(process.env.URL);
+        console.log("database connected");
+    } catch (error) {
+        console.log(error);
+    }
+
+}
